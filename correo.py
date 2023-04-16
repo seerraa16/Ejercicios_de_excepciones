50f44612a6a9207ad10b96925801e1a8f4233973
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-04-16 12:38:32 +0200

    codigo definiendo primero el formato

--- correo.py
+++ correo.py
@@ -1,3 +1,13 @@
 # Permitir entrada a app si el correo es valido
-
+def fromato_de_el_correo(correo):
+    if (r"[^@]+@[^@]+\.[^@]+", correo):
+        return True
+    else:
+        return False
+print("Ingrese su correo")
+correo = input()
+if fromato_de_el_correo(correo):
+    print("Su correo es valido")
+else:
+    print("Su correo no es valido")
 
