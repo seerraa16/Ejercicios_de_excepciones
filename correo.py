2d2e29913b1ed671fe73a4b697e1f9aeb6c32f1f
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-04-16 12:32:51 +0200

    errores y @ y .

--- correo.py
+++ correo.py
@@ -1,7 +1,17 @@
+print("¿cual es su correo?")
+correo = input()
 def formato_correo(correo):
-    if "@" in correo:
+    if "@" "." in correo:
         return True
     else:
         return False
-print("¿cual es su correo?")
-correo = input()
+if formato_correo(correo):
+    print("correo valido")
+else:
+    print("correo invalido")
+    if not "@" in correo:
+        print("falta el @")
+    if not "." in correo:
+        print("falta el .")
+    print("correo invalido")
+
