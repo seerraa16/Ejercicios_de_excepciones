9b393194d4beb7c349fe4f1b5823735a3ec3e3a9
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-04-16 12:42:24 +0200

    definimos cpara que el correo sea valido

--- correo.py
+++ correo.py
@@ -1,4 +1,10 @@
 # Permitir entrada a app si el correo es valido
 import re
+def correo_valido(correo):
+    patron = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    if re.match(patron, correo):
+        return True
+    else:
+        return False
 
 
