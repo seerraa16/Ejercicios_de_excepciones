598e4d1f58ad8e54ed09a8e7dceb7b9cb388f895
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-04-16 12:35:02 +0200

    repetir

--- correo.py
+++ correo.py
@@ -1,17 +1,3 @@
-print("¿cual es su correo?")
-correo = input()
-def formato_correo(correo):
-    if "@" "." in correo:
-        return True
-    else:
-        return False
-if formato_correo(correo):
-    print("correo valido")
-else:
-    print("correo invalido")
-    if not "@" in correo:
-        print("falta el @")
-    if not "." in correo:
-        print("falta el .")
-    print("correo invalido")
+# Permitir entrada a app si el correo es valido
+
 
